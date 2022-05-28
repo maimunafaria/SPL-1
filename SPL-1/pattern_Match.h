f04a92914f573492d278bc93780990b516491657
maimunafaria <99750670+maimunafaria@users.noreply.github.com>
2022-05-28 18:55:26 +0600

    Add files via upload

---
+++ SPL-1/pattern_Match.h
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+#define d 256
+int rabinKarp(string str, string subStr)
+{
+int n = str.length();
+  int m = subStr.length();
+  int p=0;
+  int t=0;
+  int h=1;
+  int i,j;
+  int countt=0;
+  for(i=0;i<m-1;i++)
+    {
+        h=(h*d);
+    }
+  for(i=0;i<m;i++)
+  {
+    p=(d*p+subStr[i]);
+    t=(d*t+str[i]);
+  }
+  for(i=0;i<=n-m;i++)
+  {
+    if(p==t)
+    {
+        int flag=0;
+      for(j=0;j<m;j++)
+      {
+        if(str[i+j]!=subStr[j])
+        {
+            flag=1;
+          break;
+        }
+      }
+      if(flag==0)
+      {
+        countt++;
+      //  cout<<"Similarity at: "<<i+1;
+      //  cout<<"\n";
+      }
+    }
+    if(i<n-m)
+    {
+      t=(d*(t-str[i]*h)+str[i+m]);
+    }
+  }
+  return countt;
+}
+
