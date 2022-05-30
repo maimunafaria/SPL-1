22fdb2d57abe4fc32ce50bbe7c79ba9765456583
maimunafaria <99750670+maimunafaria@users.noreply.github.com>
2022-05-30 12:53:51 +0600

    Delete SPL(main) directory

--- SPL(main)/file6/new12.c
+++
@@ -1,1 +0,0 @@
-#include<stdio.h>int main(){    int x, y, z, temp ;    scanf("%d%d%d", &x,&y,&z) ;    if(a<c) {         temp= x ;        x=z ;        z=temp ;    }    if(a<b) {         temp=x ;        x=y ;        y=temp ;    }    if(b<c) {        temp=y ;        y=z ;        z=temp ;    }    printf("%d\t%d\t%d\n",x,y,z) ;    }
--- SPL(main)/file6/new9.c
+++
@@ -1,1 +0,0 @@
-#include<stdio.h>int ans(int a){return a;}int main(){int a;scanf("%d" , a);for(int i=0;i<a;i++){ int sum =0;sum+=a;ans(sum);}}
